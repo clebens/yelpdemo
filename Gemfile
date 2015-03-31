9a8a78778ec9940812c85ebf5532a06cecb176b8
Colin Lebens <colin.lebens@gmail.com>
2015-03-30 18:05:26 -0700

    Added images

--- Gemfile
+++ Gemfile
@@ -21,6 +21,10 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'carrierwave'
+gem 'fog'
+gem 'figaro'
+gem 'mini_magick'
 
 group :development, :test do
     gem 'sqlite3'
---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+class ImageUploader < CarrierWave::Uploader::Base
+
+  include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :fog
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+  
+  process :resize_to_fit => [200, 300]
+
+end
